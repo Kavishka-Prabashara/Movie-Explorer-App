ba370230d59896b316baa43994bf7da63e39f724
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-05-08 23:52:08 +0530

    Add padding to main content to avoid NavBar overlap

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+// App.tsx
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
@@ -7,24 +8,47 @@ import MovieDetailsPage from "./pages/MovieDetailsPage";
 import UserAccount from "./pages/UserAccount";
 import NotFoundPage from "./pages/NotFoundPage";
 
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider, Box } from "@mui/material";
 import theme from "./theme";
-
 import NavBar from "./components/NavBar";
 
 const App: React.FC = () => {
+    const [selectedLanguage, setSelectedLanguage] = useState('');
+    const [selectedGenre, setSelectedGenre] = useState('');
+
+    const languages = ["English", "Sinhala", "Tamil", "Hindi"];
+    const genres = ["Action", "Comedy", "Drama", "Sci-Fi", "Thriller"];
+
+    const handleLanguageChange = (language: string) => {
+        setSelectedLanguage(language);
+        console.log(`Selected Language: ${language}`);
+    };
+
+    const handleGenreChange = (genre: string) => {
+        setSelectedGenre(genre);
+        console.log(`Selected Genre: ${genre}`);
+    };
+
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Router>
-                <NavBar />
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/movie/:id" element={<MovieDetailsPage />} />
-                    <Route path="/favorites" element={<UserAccount />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
+                <NavBar
+                    onLanguageChange={handleLanguageChange}
+                    onGenreChange={handleGenreChange}
+                    languages={languages}
+                    genres={genres}
+                />
+                {/* Add padding-top to the content below the NavBar */}
+                <Box sx={{ paddingTop: '64px' }}> {/* Adjust '64px' based on your NavBar's height */}
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/movie/:id" element={<MovieDetailsPage />} />
+                        <Route path="/user" element={<UserAccount />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </Box>
             </Router>
         </ThemeProvider>
     );

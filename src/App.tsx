b02c920f45e78dd7ed224e07a2216740d59a83dd
Kavishka-Prabashara <kavishkaprabashara@gmail.com>
2025-05-08 22:31:20 +0530

    Update App.tsx to add routing and include NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage";
+import UserAccount from "./pages/UserAccount";
+import NotFoundPage from "./pages/NotFoundPage";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
-export default App
+import NavBar from "./components/NavBar";
+
+const App: React.FC = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Router>
+                <NavBar />
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/movie/:id" element={<MovieDetailsPage />} />
+                    <Route path="/favorites" element={<UserAccount />} />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    );
+};
+
+export default App;
